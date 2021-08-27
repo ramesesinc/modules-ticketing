f769db46ff3cb48223028cbbafb54b339668b8dc
worgflores <worgflores@gmail.com>
2021-08-27 12:33:38 +0800

    res: updated

--- res/patch-etracs.sql
+++ res/patch-etracs.sql
@@ -1,2 +1,6 @@
+
+INSERT INTO `sys_domain` (`name`, `connection`) 
+VALUES ('Ticketing', 'ticketing');
+
 INSERT INTO `cashreceipt_plugin` (`objid`, `connection`, `servicename`) 
 VALUES ('ticketing', 'ticketing', 'TicketingPaymentService');
